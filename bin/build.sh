cc798267383afd04a1812091cecfcb8c050b5dfc
Anthony Lai <anybucket@outlook.com>
2018-10-25 02:12:15 +0800

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build --file Dockerfile --tag charlesstover/reverse-proxy .
+docker build --file Dockerfile --tag wechain/reverse-proxy .
 
 docker rmi $(docker images --filter dangling=true --no-trunc --quiet)
